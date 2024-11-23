3f623a01048c1fb616524a1b18273b8982b96611
StormSister <hydmet.import@gmail.com>
2024-11-23 20:42:29 +0100

    submit information from contact page using POJO object

--- src/main/java/com/monika/kindergarden/controller/ContactController.java
+++ src/main/java/com/monika/kindergarden/controller/ContactController.java
@@ -1,7 +1,10 @@
 package com.monika.kindergarden.controller;
 
+import com.monika.kindergarden.model.Contact;
+import com.monika.kindergarden.service.ContactService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ContactController {
     private static final Logger log = LoggerFactory.getLogger(ContactController.class);
+    private final ContactService contactService;
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
 
 
     @RequestMapping("/contact")
@@ -18,14 +26,20 @@ public class ContactController {
         return "contact.html";
     }
 
+//    @PostMapping(value = "/saveMsg")
+//    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email,
+//                                    @RequestParam String subject, @RequestParam String message){
+//        log.info("Name : " + name);
+//        log.info("Mobile Number : " + mobileNum);
+//        log.info("Email : " + email);
+//        log.info("Subject : " + subject);
+//        log.info("Message : " + message);
+//        return new ModelAndView("redirect:/contact");
+//    }
     @PostMapping(value = "/saveMsg")
-    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email,
-                                    @RequestParam String subject, @RequestParam String message){
-        log.info("Name : " + name);
-        log.info("Mobile Number : " + mobileNum);
-        log.info("Email : " + email);
-        log.info("Subject : " + subject);
-        log.info("Message : " + message);
+    public ModelAndView saveMessage(Contact contact){
+        contactService.saveMessageDetails(contact);
+
         return new ModelAndView("redirect:/contact");
     }
 }
