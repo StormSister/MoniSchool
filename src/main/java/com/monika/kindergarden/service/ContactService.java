a0a1859dff71e0ae87beedb6102e55a1ba1289ae
StormSister <hydmet.import@gmail.com>
2024-11-25 09:51:34 +0100

    add query params to holidays link

--- src/main/java/com/monika/kindergarden/service/ContactService.java
+++ src/main/java/com/monika/kindergarden/service/ContactService.java
@@ -1,13 +1,15 @@
 package com.monika.kindergarden.service;
 
 import com.monika.kindergarden.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ContactService {
-    private static final Logger log  = LoggerFactory.getLogger(ContactService.class);
+
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
         log.info(contact.toString());
