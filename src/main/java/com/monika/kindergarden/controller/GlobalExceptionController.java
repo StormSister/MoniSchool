c50d402c7dfbc5779ee71fd9eb87cdd7027da7cb
StormSister <hydmet.import@gmail.com>
2024-11-26 12:18:56 +0100

    controller advice + exceptionHandler to handle errors in aplication

---
+++ src/main/java/com/monika/kindergarden/controller/GlobalExceptionController.java
@@ -0,0 +1,20 @@
+package com.monika.kindergarden.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception ex) {
+        ModelAndView errorPage = new ModelAndView();
+        errorPage.setViewName("error");
+        errorPage.addObject("errormsg", ex.getMessage());
+        return errorPage;
+
+    }
+}
