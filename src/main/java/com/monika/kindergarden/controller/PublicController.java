f7258ae2ac4a3c2f746f4585a300cc90c895cc81
StormSister <hydmet.import@gmail.com>
2024-12-01 14:10:05 +0100

    implement Person, Roles and Address classes, validate Person fields using custeom validation and custom annotations

--- src/main/java/com/monika/kindergarden/controller/PublicController.java
+++ src/main/java/com/monika/kindergarden/controller/PublicController.java
@@ -2,10 +2,15 @@ package com.monika.kindergarden.controller;
 
 import com.monika.kindergarden.model.Person;
 import com.monika.kindergarden.service.PersonService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,4 +25,15 @@ public class PublicController {
         model.addAttribute("person", new Person());
         return "register.html";
     }
+
+    @PostMapping("/createUser")
+    public String createUser(@Valid@ModelAttribute("person") Person person, Errors errors){
+        if(errors.hasErrors()){
+            return "register.html";
+        }
+
+        return "redirect:/login?register=true";
+    }
+
+
 }
--- src/main/java/com/monika/kindergarden/model/Person.java
+++ src/main/java/com/monika/kindergarden/model/Person.java
@@ -1,10 +1,63 @@
 package com.monika.kindergarden.model;
 
-import jakarta.persistence.Entity;
+import com.monika.kindergarden.annotations.FieldsValueMatch;
+import com.monika.kindergarden.annotations.PasswordValidator;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Data
-public class Person {
+@FieldsValueMatch.List({
+        @FieldsValueMatch(
+                field = "pwd",
+                fieldMatch = "confirmPwd",
+                message = "Passwords do not match"
+        ),
+        @FieldsValueMatch(
+                field = "email",
+                fieldMatch = "confirmEmail",
+                message = "Email addresses do not match"
+        )
+})
+public class Person extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native")
+    private int personId;
+
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name must not be less than 3 characters")
     private String name;
+
+    @NotBlank(message = "Mobile Number must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be exactly 10 digits long")
+    private String mobileNumber;
+
+    @NotBlank(message = "Password must not be blank")
+    @Email(message = "Please provide a valid email address")
+    private String email;
+
+    @NotBlank(message = "Confirmed Email must not be blank")
+    @Email(message = "Please provide a valid confirm email address")
+    @Transient
+    private String confirmEmail;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 5, message = "Password must not be less than 5 characters")
+    @PasswordValidator
+    private String pwd;
+
+    @NotBlank(message = "Confirm Password must not be blank")
+    @Size(min = 5, message = "Confirm Password must not be less than 5 characters")
+    @Transient
+    private String confirmPwd;
+
+
+//    private Roles roles;
+//    private Address address;
 }
