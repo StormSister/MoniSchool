386e32f16ee9adadc9bbc06fb1b45d67d4202980
StormSister <hydmet.import@gmail.com>
2024-12-02 11:41:43 +0100

    disable javax validations in Spring Data JPA

--- src/main/java/com/monika/kindergarden/controller/GlobalExceptionController.java
+++ src/main/java/com/monika/kindergarden/controller/GlobalExceptionController.java
@@ -1,6 +1,8 @@
 package com.monika.kindergarden.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,12 +11,31 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class GlobalExceptionController {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ModelAndView handleValidationExceptions(MethodArgumentNotValidException ex) {
+        ModelAndView errorPage = new ModelAndView();
+        errorPage.setViewName("error");
+        StringBuilder errorMessage = new StringBuilder("Validation errors:");
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errorMessage.append(" [").append(error.getField()).append(": ").append(error.getDefaultMessage()).append("]")
+        );
+        errorPage.addObject("errormsg", errorMessage.toString());
+        return errorPage;
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ModelAndView handleDatabaseExceptions(DataIntegrityViolationException ex) {
+        ModelAndView errorPage = new ModelAndView();
+        errorPage.setViewName("error");
+        errorPage.addObject("errormsg", "Database error: " + ex.getMessage());
+        return errorPage;
+    }
+
     @ExceptionHandler(Exception.class)
     public ModelAndView handleException(Exception ex) {
         ModelAndView errorPage = new ModelAndView();
         errorPage.setViewName("error");
         errorPage.addObject("errormsg", ex.getMessage());
         return errorPage;
-
     }
 }
