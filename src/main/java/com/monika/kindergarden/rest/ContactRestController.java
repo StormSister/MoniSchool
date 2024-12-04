1b61fc7e0cf41df3ae851845a7e957699fff0f80
StormSister <hydmet.import@gmail.com>
2024-12-04 16:47:51 +0100

    save message opertion using rest service and ResponseEntity

--- src/main/java/com/monika/kindergarden/rest/ContactRestController.java
+++ src/main/java/com/monika/kindergarden/rest/ContactRestController.java
@@ -1,10 +1,14 @@
 package com.monika.kindergarden.rest;
 
 import com.monika.kindergarden.model.Contact;
+import com.monika.kindergarden.model.Response;
 import com.monika.kindergarden.repository.ContactRepository;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,5 +33,20 @@ public class ContactRestController {
         } else {
             return List.of();
         }
+    }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestHeader("invocationFrom") String invocationFrom,
+                                            @Valid @RequestBody Contact contact) {
+        log.info(String.format("Header invocationFrom = %s", invocationFrom));
+        contactRepository.save(contact);
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message saved successfully");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("isMsgSaved", "true")
+                .body(response);
+
     }
 }
