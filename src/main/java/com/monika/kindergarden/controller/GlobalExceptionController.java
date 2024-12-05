b7a0f8272c1b40f8d51faf35a0ba4b3538a46e06
StormSister <hydmet.import@gmail.com>
2024-12-05 10:19:14 +0100

    implement global error logic for REST services using @RestControllerAdvice

--- src/main/java/com/monika/kindergarden/controller/GlobalExceptionController.java
+++ src/main/java/com/monika/kindergarden/controller/GlobalExceptionController.java
@@ -2,13 +2,14 @@ package com.monika.kindergarden.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-@ControllerAdvice
+@ControllerAdvice(annotations = Controller.class)
 public class GlobalExceptionController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
