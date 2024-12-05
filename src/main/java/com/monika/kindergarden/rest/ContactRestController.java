86450a24ccd3eb679455d36d85844189f459de7f
StormSister <hydmet.import@gmail.com>
2024-12-05 11:14:21 +0100

    enable sending responses in xml format from my rest services

--- src/main/java/com/monika/kindergarden/rest/ContactRestController.java
+++ src/main/java/com/monika/kindergarden/rest/ContactRestController.java
@@ -17,12 +17,13 @@ import java.util.stream.Collectors;
 @Slf4j
 @RestController
 @RequestMapping(path = "/api/contact")
+@CrossOrigin(origins="*")
 public class ContactRestController {
 
     @Autowired
     ContactRepository contactRepository;
 
-    @GetMapping(value = "/getMessagesByStatus", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/getMessagesByStatus", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<Contact> getMessagesByStatus(@RequestParam(name = "status") String status) {
         return contactRepository.findByStatus(status);
     }
